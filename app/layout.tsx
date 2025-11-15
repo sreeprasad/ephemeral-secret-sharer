8524aba890ffb9aa0e98c685b102bbe83e43758e
sreeprasad govindankutty <sreeprasad.sp@gmail.com>
2025-11-15 11:58:24 -0800

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,12 +38,7 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning>
         <body className={`font-sans antialiased`}>
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-        >
+        <ThemeProvider>
             {children}
         </ThemeProvider>
         <Analytics />
